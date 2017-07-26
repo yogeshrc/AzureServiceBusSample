da18ac295c388d82e4ecbcc2f3784f0d9f2c558d
Yogesh Choudhary <yogesh.choudhary@capita.co.uk>
2017-07-26 14:39:53 +0530

    First commit

---
+++ WinFormPublisher/WinFormPublisher/MessagePublisher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormPublisher
+{
+    public partial class MessagePublisher : Form
+    {
+        public MessagePublisher()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = new DateTime();
+            MessageBox.Show(dateTime.ToString());
+        }
+    }
+}
