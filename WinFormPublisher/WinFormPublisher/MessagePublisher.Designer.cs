3da3064b7df9290875e4720cc1ce88e7d3c820fd
Yogesh Choudhary <yogesh.choudhary@capita.co.uk>
2017-07-26 14:42:48 +0530

    UI design

--- WinFormPublisher/WinFormPublisher/MessagePublisher.Designer.cs
+++ WinFormPublisher/WinFormPublisher/MessagePublisher.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             this.btnPublish = new System.Windows.Forms.Button();
+            this.tbxConnectionString = new System.Windows.Forms.TextBox();
+            this.lblConnectionString = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnPublish
             // 
-            this.btnPublish.Location = new System.Drawing.Point(0, 0);
+            this.btnPublish.Location = new System.Drawing.Point(12, 186);
             this.btnPublish.Name = "btnPublish";
             this.btnPublish.Size = new System.Drawing.Size(75, 23);
             this.btnPublish.TabIndex = 0;
@@ -41,21 +46,74 @@
             this.btnPublish.UseVisualStyleBackColor = true;
             this.btnPublish.Click += new System.EventHandler(this.btnPublish_Click);
             // 
+            // tbxConnectionString
+            // 
+            this.tbxConnectionString.Location = new System.Drawing.Point(12, 21);
+            this.tbxConnectionString.Name = "tbxConnectionString";
+            this.tbxConnectionString.Size = new System.Drawing.Size(391, 20);
+            this.tbxConnectionString.TabIndex = 1;
+            // 
+            // lblConnectionString
+            // 
+            this.lblConnectionString.AutoSize = true;
+            this.lblConnectionString.Location = new System.Drawing.Point(12, 5);
+            this.lblConnectionString.Name = "lblConnectionString";
+            this.lblConnectionString.Size = new System.Drawing.Size(106, 13);
+            this.lblConnectionString.TabIndex = 2;
+            this.lblConnectionString.Text = "SB Connection string";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 47);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Message to publish";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 59);
+            this.textBox1.TabIndex = 4;
+            // 
             // MessagePublisher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 221);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblConnectionString);
+            this.Controls.Add(this.tbxConnectionString);
             this.Controls.Add(this.btnPublish);
             this.Name = "MessagePublisher";
             this.Text = "Message publisher";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnPublish;
+        private System.Windows.Forms.TextBox tbxConnectionString;
+        private System.Windows.Forms.Label lblConnectionString;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
